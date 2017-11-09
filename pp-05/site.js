d5ecdaeb3eb765d0b5549e3f533928b104b7710d
Grese Hyseni <ghyseni@hawk.iit.edu>
2017-11-08 22:42:48 -0600

    Swap from audio html5 element to creating the audio element using js, on document ready

--- pp-05/site.js
+++ pp-05/site.js
@@ -1,4 +1,8 @@
-function ringDoorbell() {
-  var doorbell = new Audio('media/doorbell.mp3');
-  doorbell.play();
-}
+$(document).ready(function() {
+  $('#doorbell').remove();
+
+  function ringDoorbell() {
+    var doorbell = new Audio('media/doorbell.mp3');
+    doorbell.play();
+  }
+});
